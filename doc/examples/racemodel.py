2c21f4fe13c3f0bb334c0292ef4db84025b484d8
Richard Höchenberger <richard.hoechenberger@gmail.com>
2014-08-15 14:32:35 +0200

    Use print_function from __future__

--- doc/examples/racemodel.py
+++ doc/examples/racemodel.py
@@ -1,5 +1,6 @@
-from pphelper import racemodel
+from __future__ import print_function
 import numpy as np
+from pphelper import racemodel
 
 # Example data from Ulrich et al., 2007
 #
@@ -8,7 +9,7 @@ C = {'x': np.array([244, 249, 257, 260, 264, 268, 271, 274, 277, 291]),
      'z': np.array([234, 238, 240, 240, 243, 243, 245, 251, 254, 256, 259, 270, 280])}
 
 results = racemodel.compare_cdfs_from_raw_rts(C['x'], C['y'], C['z'])
-print results
+print(results)
 
 # Write output to an Excel file?
 saveExcel = True
@@ -16,6 +17,6 @@ saveExcel = True
 if saveExcel:
     try:
         results.to_excel('D:/Temp/Racemodel Results.xlsx')
-        print '\nExcel file successfully saved.'
+        print('\nExcel file successfully saved.')
     except:
-        print '\nCould not save Excel file!'
+        print('\nCould not save Excel file!')
