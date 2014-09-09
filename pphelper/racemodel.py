cad2d21397de5f2ea1e86fa880af29a149f90ee7
Richard Höchenberger <richard.hoechenberger@gmail.com>
2014-09-09 11:15:06 +0200

    DOC: Update docstring

--- pphelper/racemodel.py
+++ pphelper/racemodel.py
@@ -12,10 +12,12 @@
    Provides
    --------
      - ``compare_cdfs_from_raw_rts`` : Assess cumulative distribution functions from response time data and calculate the race model assumptions.
+     - ``compare_cdfs_from_dataframe`` : Assess cumulative distribution functions from response time data and calculate the race model assumptions.
      - ``gen_cdf`` : Estimate the cumulative distribution function from response time data.
      - ``gen_percentiles`` : Calculate equally spaced percentiles values.
      - ``gen_step_fun`` : Generate a step function from a set of observed response times.
      - ``get_percentiles_from_cdf`` : Get the values (response times) of a cumulative distribution function at the specified percentiles.
+     - ``calculate_statistics`` : Perform statistical tests.
 
 """
 
