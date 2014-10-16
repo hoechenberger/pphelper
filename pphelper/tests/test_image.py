8f86d95a215a4d64678dbcd3b9a740f4e4eb86dd
Richard Höchenberger <richard.hoechenberger@gmail.com>
2014-10-16 20:29:34 +0200

    ENH: Add tests for image module

--- pphelper/tests/test_image.py
+++ pphelper/tests/test_image.py
@@ -2,26 +2,27 @@
 
 from pphelper.image import lowpass_filter_image, fft_image
 import numpy as np
+import pyfftw
+import pickle
 
 
 def test_lowpass_filter_image():
     result = lowpass_filter_image(filename='data/tux.png', flatten=True)
     result_expected = np.load('data/tux_flattened_lowpass_filtered_sigma_3.npy')
-
     assert np.array_equal(result, result_expected)
 
 
 def test_fft_image():
     image = np.load('data/tux_flattened.npy')
+
+    with open('data/fftw3-wisdom.pickle', 'rb') as f:
+        pyfftw.import_wisdom(pickle.load(f))
     fft, amplitude, phase = fft_image(image)
 
     fft_expected = np.load('data/tux_flattened_fft.npy')
     amplitude_expected = np.load('data/tux_flattened_amplitude.npy')
     phase_expected = np.load('data/tux_flattened_phase.npy')
 
-    # FIXME
-    # This fails when pyfftw.interfaces.cache.enable() is called.
-    #
-    # assert np.array_equal(fft, fft_expected)
-    assert np.array_equal(amplitude, amplitude_expected)
-    assert np.array_equal(phase, phase_expected)
+    assert np.allclose(fft, fft_expected)
+    assert np.allclose(amplitude, amplitude_expected)
+    assert np.allclose(phase, phase_expected)
