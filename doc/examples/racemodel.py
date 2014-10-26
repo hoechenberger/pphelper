67a94abe5addca31da413ff88735916fdced0443
Richard Höchenberger <richard.hoechenberger@gmail.com>
2014-10-26 16:13:17 +0100

    DOC: Update example to reflect renamed function names

--- doc/examples/racemodel.py
+++ doc/examples/racemodel.py
@@ -8,7 +8,7 @@ C = {'x': np.array([244, 249, 257, 260, 264, 268, 271, 274, 277, 291]),
      'y': np.array([245, 246, 248, 250, 251, 252, 253, 254, 255, 259, 263, 265, 279, 282, 284, 319]),
      'z': np.array([234, 238, 240, 240, 243, 243, 245, 251, 254, 256, 259, 270, 280])}
 
-results = racemodel.compare_cdfs_from_raw_rts(C['x'], C['y'], C['z'])
+results = racemodel.gen_cdfs_from_raw_rts(C['x'], C['y'], C['z'])
 print(results)
 
 # Write output to an Excel file?
