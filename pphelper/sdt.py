be6bf4855d110756440391dd108a40f4a76228d4
Richard Höchenberger <richard.hoechenberger@gmail.com>
2015-02-11 16:39:14 +0100

    NF: Add A' calculation

--- pphelper/sdt.py
+++ pphelper/sdt.py
@@ -12,6 +12,7 @@ Provides
 
 from __future__ import division, unicode_literals
 from scipy.stats import norm
+import numpy as np
 
 
 def d_prime(hits, false_alarms, n, nafc=1):
@@ -49,6 +50,47 @@ def d_prime(hits, false_alarms, n, nafc=1):
     hit_rate, fa_rate = _calculate_hit_and_fa_rates(hits, false_alarms, n)
     d = norm.ppf(hit_rate) - norm.ppf(fa_rate)
     return d
+
+
+def a_prime(hits, false_alarms, n, nafc=1):
+    """
+    Calculate the sensitivity index A'.
+
+    Parameters
+    ----------
+    hits : float
+        The number of hits when detecting a signal.
+    false_alarms : float
+        The number of false alarms.
+    n : int
+        The number of trials in target and no-target trials.
+    nafc : int, optional
+        The number of alternative choices in the task. A value of ``1``
+        implies a Yes/No task.
+        Defaults to 1.
+
+    Returns
+    -------
+    A : float
+        The calculated A'.
+
+    Example
+    -------
+    >>> from pphelper import sdt
+    >>> sdt.A_prime(20, 10, 25)
+    0.79166666666666674
+
+    """
+    if nafc != 1:
+        raise NotImplementedError('Only 1-AFC implemented so far.')
+
+    hit_rate, fa_rate = _calculate_hit_and_fa_rates(hits, false_alarms, n)
+    A = 0.5 + \
+        (np.sign(hit_rate - fa_rate) *
+         ((hit_rate - fa_rate)**2 + np.abs(hit_rate - fa_rate)) /
+         (4 * np.max([hit_rate, fa_rate]) - 4 * hit_rate * fa_rate))
+
+    return A
 
 
 def criterion(hits, false_alarms, n, nafc=1):
@@ -95,7 +137,7 @@ def _calculate_hit_and_fa_rates(hits, false_alarms, n):
     hit_rate = hits / n
     fa_rate = false_alarms / n
 
-    # Adjust for exteme cases, loglinear approach
+    # Adjust for extreme cases, log-linear approach
     # http://stats.stackexchange.com/a/134802
     if (hit_rate == 0) or (hit_rate == 1) or (fa_rate == 0) or \
             (fa_rate == 1):
