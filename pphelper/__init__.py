91feca26f53428a9e8fb27debbd33b7d506a90f3
Richard Höchenberger <richard.hoechenberger@gmail.com>
2015-02-11 17:15:17 +0100

    RF: Only import hardware if nidaqmx is available

--- pphelper/__init__.py
+++ pphelper/__init__.py
@@ -5,8 +5,14 @@
     psychophysical data.
 
 """
+from __future__ import print_function, unicode_literals
 
 from .version import __version__
 from . import racemodel, utils, image
+try:
+    import nidaqmx as _nidaqmx
+except ImportError:
+    print('Problem importing the hardware module, skipping.\n'
+          'Please check if PyLibNIDAQmx is installed.')
 
 __all__ = ['racemodel', 'sdt', 'hardware', 'utils', 'image']
